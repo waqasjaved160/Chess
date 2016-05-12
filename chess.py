452f9d4aba89298f968436c07c7603c8c8be305b
Waqas Javed <waqasjaved.qc@gmail.com>
2016-05-13 02:18:44 +0500

    Added more tests for methods  - Added empty requirements file  - Tests for methods which check a piece can be placed on the board    without any harm to themselves or other pieces

--- chess.py
+++ chess.py
@@ -64,6 +64,174 @@ class HarmlessChess(object):
             if len(self.combination_of_pieces) == total_combinations:
                 break
 
+    def initialize_board(self):
+        """
+            Just placing . (dot) to all places on the board
+        """
+        self.board = [
+            ['.'] * self.board_size_x for _ in range(self.board_size_x)]
+
+    def is_empty(self, x, y):
+        """
+            If place isn't occupied by any piece
+        """
+        return self.board[x][y] == "."
+
+    def check_for_rook(self, x, y, piece):
+        """
+            Need to check two things:
+             - if the element to be inserted is not rook then there
+                should be no rook or queen on same row or column
+             - if the element to be inserted is rook there should be
+                no element on same row/column
+        :param x:
+        :param y:
+        :param piece: piece to be inserted at position x,y
+        """
+
+        if constants.ROOK in piece:
+            for i in xrange(self.board_size_x):
+                # Check on x axis on same row as piece to be placed
+                if self.board[i][y] != ".":
+                    return False
+            for i in xrange(self.board_size_x):
+                # Check on y axis on same row as piece to be placed
+                if self.board[x][i] != ".":
+                    return False
+        else:
+            for i in xrange(self.board_size_x):
+                # Check on x axis on same row as piece to be placed
+                if (constants.ROOK in self.board[i][y] or
+                        constants.QUEEN in self.board[i][y]):
+                    return False
+            for i in xrange(self.board_size_x):
+                # Check on y axis on same row as piece to be placed
+                if (constants.ROOK in self.board[x][i] or
+                        constants.QUEEN in self.board[x][i]):
+                    return False
+        return True
+
+    def check_for_bishop(self, x, y):
+        for i in xrange(self.board_size_x):
+            for j in xrange(self.board_size_y):
+                if (self.is_empty(abs(j - y), abs(i - x)) and
+                        abs(j - y) == abs(i - x) and
+                        constants.BISHOP in self.board[abs(j - y)][abs(i - x)]):
+                    return False
+        return True
+
+    def check_for_knight(self, x, y):
+        """
+            1 - If piece to be inserted is knight then there should not be
+                any rook or queen on that row / column (for this this method
+                should be called after check_for_rook())
+            2 - If piece to be inserted is knight then it should not be in
+                harm by any other knight
+            3 - By inserting this piece no other piece should be in harm
+        :param x: Position on horizontal rows
+        :param y: Position on vertical rows
+        :return:
+        """
+        for j in xrange(self.board_size_x):
+            for i in xrange(self.board_size_y):
+                if (abs(j - y) ** 2 + abs(i - x) ** 2 == 5 and
+                        constants.KNIGHT in self.board[j][i]):
+                    return False
+        return True
+
+    def check_for_king(self, x, y, piece):
+        """
+            Check if there is any king around this x,y points
+        :return: False if king is present else True
+        """
+        for j in xrange(self.board_size_x):
+            for i in xrange(self.board_size_y):
+                if abs(j - y) < 2 and abs(i - x) < 2:
+                    if j == x and i == y:
+                        continue
+                    elif (self.board[j][i] != "." and
+                            constants.ROOK not in self.board[j][i] and
+                            constants.KNIGHT not in self.board[j][i]):
+                        return False
+        return True
+
+    def in_danger(self, x, y, piece):
+        """
+            Checks whether its safe to place the piece on specific
+            position on board
+        :param x: Position on horizontal row
+        :param y: Position on vertical row
+        :param piece:
+        :return: True if in Danger else False
+        """
+        rook = self.check_for_rook(x, y, piece)
+        bishop = self.check_for_bishop(x, y)
+        knight = self.check_for_knight(x, y)
+        king = self.check_for_king(x, y, piece)
+
+        # Queen has powers of both Rook and Bishop
+
+        print "Allowed by Rook: %s, Bishop: %s, Knight: %s, King: %s" % (
+            str(rook), str(bishop), str(knight), str(king))
+        print "Will this piece be in danger: %s" % str(
+            not (rook & bishop & knight & king))
+        return not (rook & bishop & knight & king)
+
+    def place_without_harming(self, x, y, piece):
+        """
+            Move a chess piece to position x,y
+        :param x: position on board on horizontal axis
+        :param y: position on board on vertical axis
+        :param piece: Chess piece
+        :return: Boolean (True if piece is placed False otherwise)
+        """
+        if self.board[x][y] != '.':
+            # Place is already occupied
+            return False
+
+        print "Checking for in danger piece at %d,%d" % (x, y)
+
+        if not self.in_danger(x, y, piece):
+            self.board[x][y] = piece
+            print "%s placed at board[%d][%d]" % (piece, x, y)
+            print "Board position: %s" % str(self.board)
+            return True
+
+        print "Piece found to be in danger at %d,%d" % (y, x)
+        return False
+
+    def make_board(self, combination):
+        """
+            Given a combination of pieces (list) this method will try
+            to make a board of it
+        :param combination: List of pieces
+        :return: Boolean (True if board if formed False otherwise)
+        """
+        piece_placed = False
+        self.initialize_board()
+
+        print "Board initialized: %s" % self.board
+
+        for piece in combination:
+            print "Checking for piece: %s" % piece
+            for x in xrange(self.board_size_x):
+                for y in xrange(self.board_size_y):
+                    print "x,y: %d,%d" % (x, y)
+                    if self.place_without_harming(x, y, piece):
+                        piece_placed = True
+                        break
+                if piece_placed:
+                    piece_placed = False
+                    break
+        # Return True if all pieces from combination are present
+        # on board else return False
+        for piece in combination:
+            if any(piece in x_row for x_row in self.board):
+                continue
+            else:
+                return False
+        return True
+
     def main(self):
 
         while True:
@@ -92,8 +260,23 @@ class HarmlessChess(object):
             except ValueError:
                 print "Number can only be an integer"
 
-        self.make_combinations_of_pieces()
-        print self.combination_of_pieces
+        # self.make_combinations_of_pieces()
+        # print self.combination_of_pieces
+
+        # for combination in self.combination_of_pieces:
+        #     print "Combination: %s" % str(combination)
+        #     if self.make_board(combination):
+        #         print "All pieces places on board %s" % str(self.board)
+        #         self.boards.append(self.board)
+        if self.make_board(['R1', 'N3', 'N1', 'R2', 'N4', 'N2']):
+            self.boards.append(self.board)
+        # print self.boards
+        for board in self.boards:
+            for x in board:
+                print x
+            print "-" * 25
+
+        print "Total boards: %d" % len(self.boards)
 
 if __name__ == '__main__':
     cls = HarmlessChess()
--- tests.py
+++ tests.py
@@ -34,6 +34,173 @@ class HarmlessChessTests(unittest.TestCase):
 
         self.assertEqual(len(cls.combination_of_pieces), math.factorial(3))
 
+    def test_check_for_rook_with_piece_on_same_row(self):
+        """
+            When placing a piece on board
+             - If it's a rook then there should be no other piece on same
+                row or column
+             - If it's not a rook then there should be other rook or queen
+                on same row or column
+        """
+        cls = HarmlessChess()
+        cls.board_size_x = 3
+        cls.board_size_y = 3
+        cls.board = [
+            ['K2', '.', '.'],
+            ['.', '.', '.'],
+            ['.', '.', '.'],
+        ]
+        x, y = 0, 2
+
+        self.assertFalse(cls.check_for_rook(x, y, 'R1'))
+
+    def test_check_for_rook_with_no_piece_on_same_row(self):
+        """
+            When placing a piece on board
+             - If it's a rook then there should be no other piece on same
+                row or column
+             - If it's not a rook then there should be other rook or queen
+                on same row or column
+        """
+        cls = HarmlessChess()
+        cls.board_size_x = 3
+        cls.board_size_y = 3
+        cls.board = [
+            ['K2', '.', '.'],
+            ['.', '.', '.'],
+            ['.', '.', '.'],
+        ]
+        x, y = 1, 2
+
+        self.assertTrue(cls.check_for_rook(x, y, 'R1'))
+
+    def test_check_for_rook_with_two_piece_on_same_row(self):
+        """
+            When placing a piece on board
+             - If it's a rook then there should be no other piece on same
+                row or column
+             - If it's not a rook then there should be other rook or queen
+                on same row or column
+        """
+        cls = HarmlessChess()
+        cls.board_size_x = 3
+        cls.board_size_y = 3
+        cls.board = [
+            ['K2', '.', 'K1'],
+            ['.', '.', '.'],
+            ['.', '.', '.'],
+        ]
+        x, y = 2, 2
+
+        self.assertFalse(cls.check_for_rook(x, y, 'R1'))
+
+    def test_check_for_king_with_one_nearby_piece(self):
+        """
+            There should be no king with distance of one square
+            of current position
+        """
+        cls = HarmlessChess()
+        cls.board_size_x = 3
+        cls.board_size_y = 3
+        cls.board = [
+            ['K2', '.', '.'],
+            ['.', '.', '.'],
+            ['.', '.', '.'],
+        ]
+        x, y = 0, 1
+        self.assertFalse(cls.check_for_king(x, y))
+
+        x, y = 0, 2
+        self.assertTrue(cls.check_for_king(x, y))
+
+        x, y = 1, 0
+        self.assertFalse(cls.check_for_king(x, y))
+
+        x, y = 1, 1
+        self.assertFalse(cls.check_for_king(x, y))
+
+        x, y = 2, 2
+        self.assertTrue(cls.check_for_king(x, y))
+
+    def test_check_for_king_with_two_nearby_pieces(self):
+        """
+            There should be no king with distance of one square
+            of current position
+        """
+        cls = HarmlessChess()
+        cls.board_size_x = 3
+        cls.board_size_y = 3
+        cls.board = [
+            ['K2', '.', 'K1'],
+            ['.', '.', '.'],
+            ['.', '.', '.'],
+        ]
+        x, y = 0, 1
+        self.assertFalse(cls.check_for_king(x, y))
+
+        x, y = 1, 2
+        self.assertTrue(cls.check_for_king(x, y))
+
+        x, y = 1, 0
+        self.assertFalse(cls.check_for_king(x, y))
+
+        x, y = 1, 1
+        self.assertFalse(cls.check_for_king(x, y))
+
+        x, y = 2, 2
+        self.assertTrue(cls.check_for_king(x, y))
+
+        x, y = 0, 2
+        self.assertTrue(cls.check_for_king(x, y))
+
+    def test_check_for_king_with_three_pieces_on_board(self):
+        """
+            There should be no king with distance of one square
+            of current position
+        """
+        cls = HarmlessChess()
+        cls.board_size_x = 3
+        cls.board_size_y = 3
+        cls.board = [
+            ['K2', '.', 'K1'],
+            ['.', '.', '.'],
+            ['K3', '.', '.'],
+        ]
+        x, y = 0, 1
+        self.assertFalse(cls.check_for_king(x, y))
+
+        x, y = 1, 2
+        self.assertFalse(cls.check_for_king(x, y))
+
+        x, y = 1, 0
+        self.assertFalse(cls.check_for_king(x, y))
+
+        x, y = 1, 1
+        self.assertFalse(cls.check_for_king(x, y))
+
+        x, y = 2, 2
+        self.assertTrue(cls.check_for_king(x, y))
+
+    def test_check_for_knight_with_with_piece(self):
+        """
+
+        """
+        cls = HarmlessChess()
+        cls.board_size_x = 4
+        cls.board_size_y = 4
+        cls.board = [
+            ['R1', '.', '.', '.'],
+            ['.', '.', '.', '.'],
+            ['.', '.', '.', '.'],
+            ['.', '.', 'N1', '.'],
+        ]
+
+        x, y = 1, 1
+        self.assertFalse(cls.check_for_knight(x, y))
+
+        x, y = 3, 1
+        self.assertFalse(cls.check_for_knight(x, y))
+
 
 if __name__ == '__main__':
     unittest.main()
