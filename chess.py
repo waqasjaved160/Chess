d2afafd5674d7845e44b5295e1358aff7451fa38
Waqas Javed <waqasjaved.qc@gmail.com>
2016-05-12 21:57:14 +0500

    Added two files
     - tests.py for containing all unit tests for this project
     - chess.py containing the main login for this project

---
+++ chess.py
@@ -0,0 +1,18 @@
+"""
+    Author: Waqas Javed
+    Description: This module will list all chess moves provided the board size and
+                 number of each chess piece without harming any piece on the board
+"""
+
+
+class HarmlessChess(object):
+
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+if __name__ == '__main__':
+    cls = HarmlessChess()
+    cls.main()
